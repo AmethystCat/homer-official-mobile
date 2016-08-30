85918778ed31fdd098329811f9b5332d72701b95
HC <lilan1990917@gmail.com>
2016-08-30 19:05:25 +0800

    add imgs

---
+++ src/assets/images/sprite/stylesheet.sql
@@ -0,0 +1,95 @@
+.sprite {
+    background-image: url(spritesheet.png);
+    background-repeat: no-repeat;
+    display: block;
+}
+
+.sprite-s_logo {
+    width: 332px;
+    height: 42px;
+    background-position: -5px -5px;
+}
+
+.sprite-s_menu {
+    width: 63px;
+    height: 42px;
+    background-position: -347px -5px;
+}
+
+.sprite-s_pic1_QA {
+    width: 28px;
+    height: 29px;
+    background-position: -420px -5px;
+}
+
+.sprite-s_pic1_ab {
+    width: 325px;
+    height: 66px;
+    background-position: -420px -44px;
+}
+
+.sprite-s_pic1_app {
+    width: 284px;
+    height: 574px;
+    background-position: -5px -120px;
+}
+
+.sprite-s_pic1_index1 {
+    width: 374px;
+    height: 124px;
+    background-position: -299px -120px;
+}
+
+.sprite-s_pic1_index2 {
+    width: 372px;
+    height: 99px;
+    background-position: -299px -254px;
+}
+
+.sprite-s_pic1_index3 {
+    width: 178px;
+    height: 69px;
+    background-position: -299px -363px;
+}
+
+.sprite-s_pic2_QA {
+    width: 21px;
+    height: 18px;
+    background-position: -458px -5px;
+}
+
+.sprite-s_pic2_ab {
+    width: 50px;
+    height: 48px;
+    background-position: -683px -120px;
+}
+
+.sprite-s_pic2_index2 {
+    width: 328px;
+    height: 584px;
+    background-position: -755px -5px;
+}
+
+.sprite-s_pic3_ab {
+    width: 50px;
+    height: 48px;
+    background-position: -299px -599px;
+}
+
+.sprite-s_pic4_ab {
+    width: 50px;
+    height: 48px;
+    background-position: -359px -599px;
+}
+
+.sprite-s_pic5_ab {
+    width: 155px;
+    height: 152px;
+    background-position: -419px -599px;
+}
+
+.sprite-s_pic6_ab {
+    width: 147px;
+    height: 169px;
+    background-position: -584px -599px;
+}
